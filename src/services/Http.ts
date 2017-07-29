69ab7508a77ec466625516b33fc0c674d0e1d5dc
Lucio Rubens <luciorubeens@gmail.com>
2017-07-29 00:34:12 -0300

    rx-http-request to request

--- src/services/Http.ts
+++ src/services/Http.ts
@@ -4,46 +4,18 @@
 /** Http calls from peer url. */
 
 import { Observable } from 'rxjs/Observable';
-
-import { RxHR, RxHttpRequest, RxHttpRequestResponse } from '@akanass/rx-http-request';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/bindCallback';
 
-import {deserialize, serialize} from 'json-typescript-mapper';
+import { RxRequest } from './RxRequest';
+
+import { deserialize, serialize } from 'json-typescript-mapper';
 
 import * as model from '../model';
 
-/**
- * Convert property from interface to JSON
- */
-function formatParams(params: any): any {
-  if (!params) {
-    return;
-  }
-
-  const options = JSON.parse(JSON.stringify(params));
-
-  return { qs: options };
-}
-
-/**
- * Convert JSON response to specific interface.
- */
-function formatResponse(response: any, responseType: any) {
-  try {
-    let result: typeof responseType;
-    const body = typeof response.body === 'string' ? JSON.parse(response.body) : response.body;
-
-    result = deserialize(responseType, body);
-
-    return result;
-  } catch (e) {
-    throw new Error(response.body);
-  }
-}
-
 export default class Http {
 
-  private baseRequest: RxHttpRequest;
+  private baseRequest;
 
   public constructor(public network?: model.Network) {
     const options = {
@@ -59,17 +31,18 @@ export default class Http {
       };
     }
 
-    this.baseRequest = RxHR.defaults(options);
+    this.baseRequest = new RxRequest(options);
   }
 
   public getNative<T>(url: string, params: any = {}, responseType?: new() => T): Observable<T> {
-    return RxHR.get(url, formatParams(params))
-               .map((data) => formatResponse(data, responseType));
+    const r = new RxRequest({ json: true });
+
+    return r.get(url, this.formatParams(params)).map((data) => this.formatResponse(data, responseType));
   }
 
   public get<T>(url: string, params: any = {}, responseType?: new() => T): Observable<T> {
-    return this.baseRequest.get(`/api${url}`, formatParams(params))
-                           .map((data) => formatResponse(data, responseType));
+    return this.baseRequest.get(`/api${url}`, this.formatParams(params))
+                           .map((data) => this.formatResponse(data, responseType));
   }
 
   public post<T>(url: string, body: any, responseType?: new() => T): Observable<T> {
@@ -79,7 +52,7 @@ export default class Http {
     };
 
     return this.baseRequest.post(url, options)
-                           .map((data) => formatResponse(data, responseType));
+                           .map((data) => this.formatResponse(data, responseType));
   }
 
   public put(url: string, data: any) {
@@ -88,6 +61,31 @@ export default class Http {
     };
 
     return this.baseRequest.put(url, options);
+  }
+
+  /**
+   * Convert JSON response to specific interface.
+   */
+  private formatResponse(response: any, responseType: any) {
+    try {
+      let result: typeof responseType;
+      const body = typeof response === 'string' ? JSON.parse(response) : response;
+
+      result = deserialize(responseType, body);
+
+      return result;
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+
+  /**
+   * Convert property from interface to JSON
+   */
+  private formatParams(params: any): any {
+    const options = JSON.parse(JSON.stringify(params) || '{}');
+
+    return { qs: options };
   }
 
 }
---
+++ src/services/RxRequest.ts
@@ -0,0 +1,49 @@
+/**
+ * Based on @waldojeffers/rx-request
+ */
+
+import { Observable } from 'rxjs';
+import * as request from 'request';
+
+export class RxRequest {
+
+  private req: request.RequestAPI<request.Request, request.CoreOptions, request.RequiredUriUrl>;
+  public get;
+  public post;
+  public put;
+
+  constructor(options: any) {
+    this.req = request.defaults(options);
+
+    this.get = this.toObservable(this.req.get);
+    this.post = this.toObservable(this.req.post);
+    this.put = this.toObservable(this.req.put);
+  }
+
+  private toObservable(method: any): (url: string, options: request.CoreOptions) => Observable<any> {
+
+    return (url: string, options: request.CoreOptions): Observable<any> => {
+      return Observable.create((observer) => {
+        let body = '';
+
+        method(url, options)
+          .on('response', function onResponse(res) {
+            res.setEncoding('utf8');
+            if (res.statusCode < 200 || res.statusCode >= 300) {
+              this.emit('error', {
+                statusCode: res.statusCode,
+                statusMessage: res.statusMessage,
+              });
+            }
+          })
+
+          .on('error', (e) => observer.error(e))
+          .on('data', (chunk) => body += chunk)
+          .on('end', () => {
+            observer.next(body);
+            observer.complete();
+          });
+      });
+    };
+  }
+}
