1473df3ee11859625a97a3044570f845a89eaa21
Yago Antonio <yago_antonio@live.com>
2017-08-22 19:57:27 -0300

    Update PeerApi.ts

--- src/api/PeerApi.ts
+++ src/api/PeerApi.ts
@@ -12,7 +12,43 @@ import config from '../config';
 export default class PeerApi {
 
   constructor(private http: Http) {}
+  
+  /**
+  * Find good peer ordered by synchronized blocks.
+  */
+  public static findGoodPeer(): Observable<model.Peer> {
+    const http = new Http(network);
+    return Observable.create((observer) => {
+      const networkType = model.NetworkType[http.network.type].toLowerCase();
+      const peersList = config.networks[networkType].peers;
 
+      const blockList = [];
+
+      const loader = new LoaderApi(http);
+
+      peersList.forEach((element, index) => {
+        loader
+          .synchronisationStatus(`http://${element}`)
+          .subscribe((status) => {
+            blockList.push([element, status.blocks]);
+
+            // when find a good peer or at the end
+            if (status.blocks === 0 || peersList.length - 1 === index) {
+              blockList.sort((a, b) => a[1] < b[1] ? 1 : -1); // sort by better to the worst
+              const host = blockList[0][0].split(':');
+
+              const peer: model.Peer = new model.Peer;
+              peer.ip = host[0];
+              peer.port = host[1];
+
+              observer.next(peer);
+              observer.complete();
+            }
+          }, (e) => Observable.empty());
+      });
+    });
+  }
+  
   /**
    * Get peer by ip and port.
    */
@@ -31,7 +67,8 @@ export default class PeerApi {
   /**
    * Find good peer ordered by synchronized blocks.
    */
-  public findGoodPeer(): Observable<model.Peer> {
+  
+  public findGoodPeer(network: model.Network): Observable<model.Peer> {    
     return Observable.create((observer) => {
       const networkType = model.NetworkType[this.http.network.type].toLowerCase();
       const peersList = config.networks[networkType].peers;
@@ -61,6 +98,6 @@ export default class PeerApi {
           }, (e) => Observable.empty());
       });
     });
-  }
-
+  } 
+    
 }
